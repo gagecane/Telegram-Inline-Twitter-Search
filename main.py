91ee74171b49b474f68c0f1d53c9180e249abe92
Gage Cane <45441614+gagecane@users.noreply.github.com>
2020-06-25 23:37:17 -0700

    added pytest example

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Python-based Telegram bot for @stoobs_ebooks_bot
+
+python-telegram-bot documentation: https://python-telegram-bot.readthedocs.io/en/stable/
+
+"""
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
